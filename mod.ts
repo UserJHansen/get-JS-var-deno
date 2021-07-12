fdb50178200ee9c68b3b1e700ca3131d989cd76d
UserJHansen <47265397+UserJHansen@users.noreply.github.com>
2021-07-12 13:45:12 +1000

    ✏️ Fix Formating

--- mod.ts
+++ mod.ts
@@ -1,26 +1,42 @@
 function SeperateLines(input: string, split = "\n", trim = true): string[] {
-    return trim ? input.split(split).map(x=>x.trim()) : input.split(split);
-}  
-
-export function FindVariable(target: string, script: string, split = "\n", trim = true) {
-    var ScriptArray = SeperateLines(script, split, trim)
-    var variableRegex = new RegExp(`${target}(?: *= *)(.+)(?=;?)`)
-
-    var result = ""
-    for (const lineNo in ScriptArray)
-     result = variableRegex.exec(ScriptArray[lineNo])?.[1].replace(/;$/, "") || result;
-
-    return result
+  return trim ? input.split(split).map((x) => x.trim()) : input.split(split);
 }
 
-export function FindVariableArray(target: string, script: string, split = "\n", trim = true) {
-    var ScriptArray = SeperateLines(script, split, trim)
-    var variableRegex = new RegExp(`${target}(?: *= *)(.+)(?=;?)`)
+export function FindVariable(
+  target: string,
+  script: string,
+  split = "\n",
+  trim = true,
+) {
+  var ScriptArray = SeperateLines(script, split, trim);
+  var variableRegex = new RegExp(`${target}(?: *= *)(.+)(?=;?)`);
 
-    var result = []
-    for (const lineNo in ScriptArray)
-        if (variableRegex.exec(ScriptArray[lineNo])?.[1].replace(/;$/, ""))
-            result.push(variableRegex.exec(ScriptArray[lineNo])?.[1].replace(/;$/, ""))
+  var result = "";
+  for (const lineNo in ScriptArray) {
+    result = variableRegex.exec(ScriptArray[lineNo])?.[1].replace(/;$/, "") ||
+      result;
+  }
 
-    return result
+  return result;
+}
+
+export function FindVariableArray(
+  target: string,
+  script: string,
+  split = "\n",
+  trim = true,
+) {
+  var ScriptArray = SeperateLines(script, split, trim);
+  var variableRegex = new RegExp(`${target}(?: *= *)(.+)(?=;?)`);
+
+  var result = [];
+  for (const lineNo in ScriptArray) {
+    if (variableRegex.exec(ScriptArray[lineNo])?.[1].replace(/;$/, "")) {
+      result.push(
+        variableRegex.exec(ScriptArray[lineNo])?.[1].replace(/;$/, ""),
+      );
+    }
+  }
+
+  return result;
 }
